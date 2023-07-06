e884eee5b71b005319e71733e87140d19dd44606
njagiiii <grace.makena@student.moringaschool.com>
2023-07-06 00:11:19 -0400

    completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModel = document.getElementById('modal') // Look for the error model
+errorModel.classList.add('hidden') // add class hidden to error model
 
 
+const heartClicked = (event) => { // events created for the hearts
 
+  mimicServerCall() 
+  .then(()=>{ 
+    if(event.target.textContent == EMPTY_HEART){  
+      event.target.textContent = FULL_HEART       
+      event.target.classList.add('activated-heart') 
+    }else{
+      event.target.textContent = EMPTY_HEART      
+      event.target.classList.remove('activated-heart')  
+    }
+
+  })
+  .catch(()=>{  // if server is reject
+    errorModel.classList.remove('hidden')  
+    setTimeout(function(){  
+      errorModel.classList.add('hidden') 
+    }, 3000)
+  })
+
+}
+
+let hearts = document.getElementsByClassName('like-glyph')
+for (let heart of hearts) {
+  heart.addEventListener('click', heartClicked) 
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
